81532f06737f74163eb4ef448ad5e390e2279095
oskza <dev@oskza.com>
2025-10-15 18:46:15 +0300

    Imported google fonts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="home">
   <h1>Home</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, neque quod enim necessitatibus repellendus sequi perspiciatis molestiae ducimus nam nobis sed amet ipsa quas laboriosam unde nostrum illum et! Deserunt dolorum distinctio, repellat aspernatur fuga nemo unde perferendis ullam vero voluptatibus libero saepe quis eius impedit perspiciatis! Molestiae, quia unde?</p>
 </div>
 </template>
