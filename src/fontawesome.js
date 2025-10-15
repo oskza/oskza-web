40a32a879178eebfc046e35353d1881942d13778
oskza <dev@oskza.com>
2025-10-15 19:00:23 +0300

    Set up fontawesome icons

---
+++ src/fontawesome.js
@@ -0,0 +1,9 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faSun } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faSun, faGithub)
+
+export { FontAwesomeIcon }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import router from './router'
+
 import './style.css'
 import App from './App.vue'
 
+import { FontAwesomeIcon } from './fontawesome'
+
 createApp(App)
   .use(router)
+  .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app')
