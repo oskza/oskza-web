185215a1a3e5d751cbb4d48707c2e0b0322a7e77
oskza <dev@oskza.com>
2025-10-15 23:39:05 +0300

    Created InternalLink component

---
+++ src/components/ui/InternalLink.vue
@@ -0,0 +1,23 @@
+<template>
+<router-link
+  class="internal-link"
+  :to="linkTo">
+  <slot></slot>
+</router-link>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const { routeName, queryKey, queryValue } = defineProps({
+  routeName: { type: String, required: true },
+  queryKey: String,
+  queryValue: String
+})
+
+const linkTo = computed(() => {
+  return (queryKey && queryValue)
+    ? { name: routeName, query: { [queryKey]: queryValue } }
+    : { name: routeName };
+})
+</script>
