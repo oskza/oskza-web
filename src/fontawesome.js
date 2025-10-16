0bf675f2696e3a11ae03460d00199cb0f8f51811
oskza <dev@oskza.com>
2025-10-16 11:14:12 +0300

    Imported icons for theme switcher component

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -1,9 +1,13 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faArrowUp, faArrowLeft, faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
+import {
+  faArrowUp, faArrowLeft, faEnvelopeSquare,
+  faSun, faMoon
+} from '@fortawesome/free-solid-svg-icons'
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faArrowUp, faArrowLeft, faEnvelopeSquare, faGithubSquare, faLinkedin)
+library.add(faArrowUp, faArrowLeft, faEnvelopeSquare,
+            faSun, faMoon, faGithubSquare, faLinkedin)
 
 export { FontAwesomeIcon }
