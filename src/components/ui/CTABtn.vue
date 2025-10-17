01c9ef1a23e1773639a0301db1f09e95efe33620
oskza <dev@oskza.com>
2025-10-17 23:34:03 +0300

    Created CTABtn and CTABtnList components

---
+++ src/components/ui/CTABtn.vue
@@ -0,0 +1,46 @@
+<template>
+<AppBtn class="cta-btn" v-bind="filteredProps" pill>
+  <font-awesome-icon v-if="icon" class="icon" :icon="icon" />
+  <span class="text"><slot></slot></span>
+</AppBtn>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import AppBtn from './AppBtn.vue'
+
+const props = defineProps({
+  color: String,
+  icon: [ Array, String ],
+  onClick: Function,
+  url: String,
+  external: Boolean,
+  outline: Boolean
+})
+
+const filteredProps = computed(() => {
+  const { icon, ...rest } = props
+  return rest
+})
+</script>
+
+<style scoped>
+.cta-btn {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: var(--spacing-md) var(--spacing-xl);
+  font-family: var(--font-stack-mono);
+  font-weight: 700;
+}
+
+.cta-btn.btn-primary.btn-outline { color: var(--text-color); }
+
+.icon {
+  position: absolute;
+  left: var(--spacing-xl);
+  padding-right: var(--spacing-sm);
+  border-right: 1.5px solid var(--border-color);
+}
+</style>
---
+++ src/components/ui/CTABtnList.vue
@@ -0,0 +1,26 @@
+<template>
+<ul class="cta-btn-list">
+  <li>
+    <CTABtn color="accent" url="#contact" icon="fa-solid fa-envelope">
+      {{ $t('btns.contact') }}
+    </CTABtn>
+  </li>
+  <li>
+    <CTABtn color="primary" icon="fa-solid fa-file-pdf" url="/resume_el.pdf" external outline>
+      {{ $t('btns.resume') }}
+    </CTABtn>
+  </li>
+</ul>
+</template>
+
+<script setup>
+import CTABtn from './CTABtn.vue'
+</script>
+
+<style scoped>
+.cta-btn-list {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
