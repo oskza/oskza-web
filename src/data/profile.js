d183dfbb91484440c558feb1b3a43dcd1d1e1200
oskza <dev@oskza.com>
2025-10-27 13:37:04 +0200

    Added more profile data

--- src/data/profile.js
+++ src/data/profile.js
@@ -1,6 +1,15 @@
 export const profile = {
-  email: 'info@oskza.com',
   github: 'https://github.com/oskza',
   linkedin: 'https://www.linkedin.com/in/oskza',
-  locationMap: 'https://maps.app.goo.gl/Fre5dr1fLmKRWAaT9'
+  locationMap: 'https://maps.app.goo.gl/Fre5dr1fLmKRWAaT9',
+  email: {
+    info: 'info@oskza.com',
+    courses: 'courses@oskza.com'
+  },
+  facebook: {
+    courses: 'https://www.facebook.com/oskzacourses'
+  },
+  instagram: {
+    courses: 'https://www.instagram.com/oskzacourses'
+  }
 }
