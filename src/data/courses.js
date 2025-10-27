b69e0597120f80376eec171a99981eda86acf567
oskza <dev@oskza.com>
2025-10-27 13:37:24 +0200

    Added courses data

---
+++ src/data/courses.js
@@ -0,0 +1,30 @@
+export const courses = [
+  {
+    id: 'e7c2f546',
+    slug: 'programming'
+  },
+  {
+    id: 'f8f629ea',
+    slug: 'computer-networks'
+  },
+  {
+    id: 'dff62161',
+    slug: 'intro-computer-science'
+  },
+  {
+    id: 'd6071694',
+    slug: 'algorithms'
+  },
+  {
+    id: 'bc9676d3',
+    slug: 'databases'
+  },
+  {
+    id: 'k3a23e01',
+    slug: 'special-topics'
+  },
+  {
+    id: 'hge14j5t',
+    slug: 'lab-support'
+  }
+]
