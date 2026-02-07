0aa1d512196fed94e52ea2b72e1c13a1558e1ab5
Zach Oskoidis <oskzadev@gmail.com>
2026-02-07 18:08:47 +0200

    Update links.js

--- src/data/links.js
+++ src/data/links.js
@@ -1,12 +1,12 @@
 export const profileLinks = {
-  email: 'info@oskza.com',
+  email: 'osko.zach@gmail.com',
   linkedin: 'https://linkedin.com/in/oskza',
   github: 'https://github.com/oskza',
   locationMap: 'https://maps.app.goo.gl/Fre5dr1fLmKRWAaT9'
 }
 
 export const courseLinks = {
-  email: 'courses@oskza.com',
+  email: 'osko.zach@gmail.com',
   facebook: 'https://facebook.com/oskzacourses',
   instagram: 'https://instagram.com/oskzacourses'
 }
